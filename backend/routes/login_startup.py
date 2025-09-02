372a3dce7b2dee2a3e7148657c0d7e0acc485fc2
udayan-majumder <udayanmajumder1983@gmail.com>
2025-09-02 21:25:08 +0530

    1.Added redis and configured db , 2.modified login,userdetails , 3.intregrated redis for fasert performance

--- backend/routes/login_startup.py
+++ backend/routes/login_startup.py
@@ -1,15 +1,33 @@
 from flask import Blueprint,request,jsonify
 from db.db import db_main
-
+from redisDB.redisdb import r
+import json
 startup_bp_login = Blueprint("loginstartup",__name__)
 startup_collection = db_main["startup"]
 
 @startup_bp_login.route("/loginstartup",methods=["POST"])
+
+#login fucntion
 def LoginStartup():
+  #body
   data = request.json
+
+  #hitting cache
+  checkCache = r.get(f"user:{data['Email']}")
+
+  #cache hit
+  if checkCache:
+      if json.loads(checkCache.decode())["Password"] == data["Password"]:
+          return jsonify({"message":"success","id":json.loads(checkCache.decode())["_id"]})
+  
+  #hitting database
   result = startup_collection.find_one({"CompanyEmail":data["Email"]})
+  
+  #if user found
   if result:
+    result["_id"] = str(result["_id"])
     if result["Password"] == data["Password"]:
-       return jsonify({"message":"success","id":str(result["_id"])})
+       r.set(f"user:{result['CompanyEmail']}",json.dumps(result),ex=600)
+       return jsonify({"message":"success","id":result["_id"]})
   else:
     return jsonify({"message":"wrong email or pass"})  
--- backend/routes/userdetails_startup.py
+++ backend/routes/userdetails_startup.py
@@ -1,12 +1,28 @@
 from flask import Blueprint,jsonify,request
 from db.db import db_main
 from bson import ObjectId
+from redisDB.redisdb import r
+import json
+
 userdetails_bp_startup = Blueprint("startupdetails",__name__)
 startup_collection = db_main["startup"]
 
+#function for user details
 @userdetails_bp_startup.route("/userdetailsstartup",methods=["POST"])
 def UserDetailsStartup():
+    #body
     data = request.json
+
+    #hit cache
+    for key in r.scan_iter("user:*"):
+        res = json.loads(r.get(key).decode())
+
+        #if user found
+        if res["_id"] == data["token"]:
+            return jsonify({"message":res})
+
+
+    #find user in database
     user = startup_collection.find_one({"_id":ObjectId(data["token"])})
     if user:
         user["_id"] = str(user["_id"])
